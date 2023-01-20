b6dbabf454de795b3237338922b94e6b15c7077f
batoolfatima1 <batool12547@gmail.com>
2023-01-21 00:54:29 +0500

    latest

---
+++ MaximumSubArray.java
@@ -0,0 +1,16 @@
+public class MaximumSubArray {
+   public static void main(String[] args)
+   {
+    int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int sum=0;
+    int maxi = arr[0];
+    for(int i=0; i< arr.length; i++)
+    {
+        sum += arr[i];
+        maxi = Math.max(maxi,sum);
+        sum = (sum < 0)? 0 : sum;
+    }
+    System.out.println(maxi);
+   } 
+}
