3ed41d18a3d87f02c7cb5983cdc9460b6943daf8
batoolfatima1 <batool12547@gmail.com>
2023-01-26 18:18:38 +0500

    latest

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -14,11 +14,15 @@ public class PascalsTriangle {
 
         else
         {
-            return pascal(i-1,j-1)+pascal(i-1,j); 
+            return pascal(i-1,j-1) + pascal(i-1,j); 
         } 
     }
+
+
+
     public static void main(String[] args) {
-        int numRows = 29;
+        int numRows = 5;
+
 
         List<List<Integer>> output = new ArrayList<List<Integer>>();
         for(int i=0; i<numRows; i++)
@@ -26,14 +30,38 @@ public class PascalsTriangle {
             List<Integer> l = new ArrayList<Integer>();
             for(int j=0; j<= i; j++)
             {
-               l.add(pascal(i, j));
+               if(i==0 || i==j || j==0)
+               {
+                l.add(1);
+               }
+               else
+               {
+                l.add(output.get(i-1).get(j-1) + output.get(i-1).get(j));
+               }
             }
             output.add(l);
         }
-
         for(List<Integer> l : output)
         {
             System.out.println(l);
         }
+
+
+
+        // List<List<Integer>> output = new ArrayList<List<Integer>>();
+        // for(int i=0; i<numRows; i++)
+        // {
+        //     List<Integer> l = new ArrayList<Integer>();
+        //     for(int j=0; j<= i; j++)
+        //     {
+        //        l.add(pascal(i, j));
+        //     }
+        //     output.add(l);
+        // }
+
+        // for(List<Integer> l : output)
+        // {
+        //     System.out.println(l);
+        // }
     }
 }
