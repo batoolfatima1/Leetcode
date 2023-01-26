958b4ee9c367e41be8faa8016b8aa547645f6120
batoolfatima1 <batool12547@gmail.com>
2023-01-27 01:06:15 +0500

    latest

---
+++ CycleInLinkedList.java
@@ -0,0 +1,55 @@
+public class CycleInLinkedList {
+ class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+             next = null;
+         }
+     }
+    public boolean hasCycle(ListNode head)
+    {
+        // brute force solution
+        // it will work only if list has unique elements
+
+        // HashSet<Integer> set = new HashSet<>();
+
+        // while(head != null)
+        // {
+        //     if(set.contains(head.val))
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         set.add(head.val);
+        //         head = head.next;
+        //     }
+
+        // }
+        // return false;
+
+
+        // two pointers approach
+
+        // if there is a  loop two pointers must meet each other
+
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(slow != fast)
+        {
+            if(fast == null || fast.next == null)
+            {
+                return false;
+            }
+            slow = slow.next;
+            fast= fast.next.next;
+        }
+        return true;
+
+    }
+}
